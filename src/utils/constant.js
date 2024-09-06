e27def3214eec5c9ba9b8c2cf0ce712982774511
Vidhi <vidhi2005ajmera@gmail.com>
2024-09-06 22:41:21 +0530

    MERN E_Commerce Frontend

---
+++ src/utils/constant.js
@@ -0,0 +1,11 @@
+export const USER_API_END_POINT =
+  "https://jobportal-ky47.onrender.com/api/v1/user";
+// https://jobportal-ky47.onrender.com/api/v1/user/login
+export const JOB_API_END_POINT =
+  "https://jobportal-ky47.onrender.com/api/v1/job";
+
+export const APPLICATION_API_END_POINT =
+  "https://jobportal-ky47.onrender.com/api/v1/application";
+
+export const COMPANY_API_END_POINT =
+  "https://jobportal-ky47.onrender.com/api/v1/company";
